29a5e505e95a2515a7a6fa3ecb15b32c96134504
Azamat <smagzanoff@gmail.com>
2024-12-06 14:24:53 +0500

    feat: Item service adding

--- internal/service/item_service.go
+++ internal/service/item_service.go
@@ -1,5 +1,31 @@
 package service
 
-func ItemService() {
+import (
+	_ "context"
+	_ "errors"
+	"fmt"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"itfest/internal/models"
+	"itfest/internal/repository"
+	"itfest/internal/utils"
+)
 
+func CreateItem(conn *pgxpool.Conn, item models.Item, file []byte) (string, error) {
+	imageKey := fmt.Sprintf("items/%s.jpg", item.Title)
+	imageURL, err := utils.UploadFile(imageKey, file)
+	if err != nil {
+		return "", err
+	}
+
+	item.ImageURL = imageURL
+	itemID, err := repository.CreateItem(conn, item)
+	if err != nil {
+		return "", err
+	}
+
+	return itemID, nil
+}
+
+func GetItemById(conn *pgxpool.Pool, id string) (*models.Item, error) {
+	return repository.GetItemById(conn, id)
 }
